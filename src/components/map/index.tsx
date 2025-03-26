34825507e9e6a66fa72adb0f8784b45f2b434c4c
Reed Markham <reedmarkham@gmail.com>
2025-03-26 18:13:50 -0400

    better logging for map creation

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -19,7 +19,9 @@ const Map: React.FC<MapProps> = ({ isClient, mapData, data }) => {
 
   useEffect(() => {
     if (isClient && typeof window !== "undefined") {
+      console.log("Importing Leaflet...");
       import("leaflet").then((leaflet) => {
+        console.log("Leaflet imported.");
         setL(leaflet);
       });
     }
@@ -27,13 +29,17 @@ const Map: React.FC<MapProps> = ({ isClient, mapData, data }) => {
 
   useEffect(() => {
     if (L && !mapRef.current) {
+      console.log("Initializing map...");
       mapRef.current = L.map("map").setView([38.9072, -77.0369], 12);
-  
+      console.log("Map initialized.");
+
       L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(mapRef.current);
+      console.log("Tile layer added to map.");
     }
 
     if (L && mapRef.current) {
       const map = mapRef.current;
+      console.log("Clearing existing layers...");
       
       // Clear existing layers (except the base tile layer)
       map.eachLayer((layer) => {
@@ -41,24 +47,31 @@ const Map: React.FC<MapProps> = ({ isClient, mapData, data }) => {
           map.removeLayer(layer);
         }
       });
+      console.log("Existing layers cleared.");
 
       if (mapData.length > 0) {
+        console.log("Setting map bounds...");
         const bounds = L.geoJSON(mapData).getBounds();
         map.fitBounds(bounds);
+        console.log("Map bounds set.");
       }
 
+      console.log("Adding points to map...");
       data.forEach((point) => {
         L.circle([point.y, point.x], {
           color: point.result === 0 ? "#56A0D3" : "#003B5C",
           radius: 50,
         }).addTo(map);
       });
+      console.log("Points added to map.");
     }
 
     return () => {
       if (mapRef.current) {
+        console.log("Removing map...");
         mapRef.current.remove();
         mapRef.current = null;
+        console.log("Map removed.");
       }
     };
   }, [L, mapData, data]);
