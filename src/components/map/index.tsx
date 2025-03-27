858b211d5858e059c61029bae21bb01db3db228e
Reed Markham <reedmarkham@gmail.com>
2025-03-26 22:07:50 -0400

    map styling

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -31,7 +31,11 @@ const Map: React.FC<MapProps> = ({ isClient, mapData, data }) => {
     // Only initialize if map doesn't already exist
     if (!mapRef.current) {
       mapRef.current = L.map("bottomMap").setView([38.9072, -77.0369], 12);
-      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(mapRef.current);
+      L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>',
+        subdomains: "abcd",
+        maxZoom: 19,
+      }).addTo(mapRef.current);
     }
 
     if (mapRef.current) {
